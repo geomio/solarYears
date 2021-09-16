91e352131d182d1b36c5e50be6dadb579bb4524e
Geoffry Millner <geoff.millner22@gmail.com>
2021-09-16 15:22:23 -0700

    test for MainControl is made and working import fixed so is syntax in main controll

---
+++ __tests__/solar.test.js
@@ -0,0 +1,22 @@
+import MainControl from "../src/solar";
+
+describe('MainControl', () => {
+
+  test('MainControl should create a object that holds planet objects with days to rotate around the sun and user object with year age', () => {
+    const system = new MainControl();
+    system.userData.userName = "jim";
+    system.userData.userAge = 25;
+    expect(system.userData.userName).toEqual('jim');
+    expect(system.userData.userAge).toEqual(25);
+    expect(system.solarSystem.mercury).toEqual(87.965);
+    expect(system.solarSystem.venus).toEqual(224.475);
+    expect(system.solarSystem.earth).toEqual(365);
+    expect(system.solarSystem.mars).toEqual(686.565);
+    expect(system.solarSystem.jupiter).toEqual(4328.9);
+    expect(system.solarSystem.saturn).toEqual(10752.9);
+    expect(system.solarSystem.uranus).toEqual(30663.65);
+    expect(system.solarSystem.neptune).toEqual(60152);
+    expect(system.solarSystem.pluto).toEqual(90736);
+    expect(system.solarSystem.eris).toEqual(203670);
+  });
+});
--- src/solar.js
+++ src/solar.js
@@ -1,21 +1,24 @@
 export default class MainControl {
   constructor(name) {
+
     this.solarSystem = {
-      mercury = 87.965,
-      venus = 224.475,
-      earth = 365,
-      mars = 686.565,
-      jupiter = 4328.9,
-      saturn = 10752.9,
-      uranus = 30663.65,
-      neptune = 60152,
-      pluto = 90736,
-      eris = 203670,
+      mercury: 87.965,
+      venus: 224.475,
+      earth: 365,
+      mars: 686.565,
+      jupiter: 4328.9,
+      saturn: 10752.9,
+      uranus: 30663.65,
+      neptune: 60152,
+      pluto: 90736,
+      eris: 203670
     }
+
     this.userData = {
-      userName = name,
-      userAge = 0,
+      userName: name,
+      userAge: 0
     }
+
   };
 
 };
