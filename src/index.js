89ad9e450e129e3b26820c8aa4988dacb5b893e7
Geoffry Millner <geoff.millner22@gmail.com>
2021-09-23 14:21:24 -0700

    added forms with planet selections and a area to enter age and submit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import SolarSystem from './src/solar.js';
+import MainControl from './src/maincontrol.js';
 
 $(document).ready(function () {
-  $('').submit(function (event) {
+  $('#userForm').submit(function (event) {
+    console.log("hi");
     event.preventDefault();
   });
 });
