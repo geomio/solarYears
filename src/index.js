002d5d4be750538f8d38a38062fa3f5869ffee67
Geoffry Millner <geoff.millner22@gmail.com>
2021-09-27 14:45:26 -0700

    added if statment to index.js that will show user output

--- src/index.js
+++ src/index.js
@@ -4,14 +4,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import MainControl from './maincontrol.js';
 
-displayToUser
+
 
 $(document).ready(function () {
   $('#submitUserData').click(function (event) {
     const userAge = $('#age').val();
-    const userSelection = $('#length2').val();
-    let mainControl = new MainControl(userAge, userSelection)
-    alert("hello");
+    const userSelection = $("input[name='mainUserInputs']:checked").val();
+    const userAgeNum = parseInt(userAge);
+    const userSelectionNum = parseInt(userSelection);
+    let mainControl = new MainControl(userAgeNum, userSelectionNum)
+    mainControl.ageToDays();
+    mainControl.runSelected();
+    if (userSelectionNum === 1) {
+      $('#mercuryInfo').show();
+    } if (userSelectionNum === 2) {
+      $('#venusInfo').show();
+    } if (userSelectionNum === 3) {
+      $('#earthInfo').show();
+    } if (userSelectionNum === 4) {
+      $('#marsInfo').show();
+    } if (userSelectionNum === 5) {
+      $('#jupiterInfo').show();
+    } if (userSelectionNum === 6) {
+      $('#saturnInfo').show();
+    } if (userSelectionNum === 7) {
+      $('#uranusInfo').show();
+    } if (userSelectionNum === 8) {
+      $('#neptuneInfo').show();
+    } if (userSelectionNum === 9) {
+      $('#plutoInfo').show();
+    } if (userSelectionNum === 10) {
+      $('#erisInfo').show();
+    } if (userSelectionNum === 11) {
+      $('#mercuryInfo').show();
+      $('#venusInfo').show();
+      $('#earthInfo').show();
+      $('#marsInfo').show();
+      $('#jupiterInfo').show();
+      $('#saturnInfo').show();
+      $('#uranusInfo').show();
+      $('#neptuneInfo').show();
+      $('#plutoInfo').show();
+      $('#erisInfo').show();
+    };
     event.preventDefault();
   });
 });
